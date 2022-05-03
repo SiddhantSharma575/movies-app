2f14d8152364af306efb916882a17c45b6acef98
Siddhant <siddhantsharma909@gmail.com>
2022-05-03 12:33:25 +0530

    Navbar added

---
+++ src/components/Bannner.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Bannner extends Component {
+  render() {
+    return <div>Bannner</div>;
+  }
+}
+
+export default Bannner;
---
+++ src/components/MovieList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class MovieList extends Component {
+  render() {
+    return (
+      <div>MovieList</div>
+    )
+  }
+}
+
+export default MovieList
